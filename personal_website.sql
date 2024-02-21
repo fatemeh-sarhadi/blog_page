83570c92d0bcbce488e389aecded95c0b8ebf2a8
Fatemeh Sarhadi <rahasdi80@gmail.com>
2024-02-21 13:49:23 +0330

    Add picture & Complete the database

--- personal_website.sql
+++ personal_website.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 21, 2024 at 08:45 AM
+-- Generation Time: Feb 21, 2024 at 11:17 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -49,6 +49,68 @@ CREATE TABLE `galleries` (
   `caption` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
 
+--
+-- Dumping data for table `galleries`
+--
+
+INSERT INTO `galleries` (`id`, `link`, `portfolio_id`, `caption`) VALUES
+(1, 'assets/img/7.png', 1, 'در این قسمت تکنولوژی های بکار رفته قرار دارد.'),
+(2, 'assets/img/10.1.png', 1, 'این یک فرم ارسال رزومه است .'),
+(3, 'assets/img/9.1.png', 1, 'این صفحه پروژه های دنجی است .'),
+(4, 'assets/img/2.png', 1, ' این صفحه توضیحات درباره دنجی است.'),
+(5, 'assets/img/5.1.png', 1, ' نمونه کارهای دنجی در این صفحه است.'),
+(6, 'assets/img/11.1.png', 1, 'در این صفحه رضایت مشتری قرار دارد .'),
+(7, 'assets/img/7.png', 2, 'در این قسمت تکنولوژی های بکار رفته قرار دارد.'),
+(8, 'assets/img/10.1.png', 2, 'این یک فرم ارسال رزومه است .'),
+(9, 'assets/img/9.1.png', 2, 'این صفحه پروژه های دنجی است .'),
+(10, 'assets/img/2.png', 2, ' این صفحه توضیحات درباره دنجی است.'),
+(11, 'assets/img/5.1.png', 2, 'نمونه کارهای دنجی در این صفحه است.'),
+(12, 'assets/img/11.1.png', 2, ''),
+(13, 'assets\\img\\1-a.jpg', 2, ''),
+(14, 'assets\\img\\2-a.jpg', 2, ''),
+(15, 'assets\\img\\3-a.jpg', 2, ''),
+(16, 'assets\\img\\4-a.jpg', 2, ''),
+(17, 'assets\\img\\5-a.jpg', 2, ''),
+(18, 'assets\\img\\6-a.jpg', 2, ''),
+(20, 'assets/img/7.png', 1, 'در این قسمت تکنولوژی های بکار رفته قرار دارد.'),
+(21, 'assets/img/10.1.png', 1, 'این یک فرم ارسال رزومه است .'),
+(22, 'assets/img/9.1.png', 1, 'این صفحه پروژه های دنجی است .'),
+(23, 'assets/img/2.png', 1, ' این صفحه توضیحات درباره دنجی است.'),
+(24, 'assets/img/5.1.png', 1, ' نمونه کارهای دنجی در این صفحه است.'),
+(25, 'assets/img/11.1.png', 1, 'در این صفحه رضایت مشتری قرار دارد .'),
+(26, 'assets/img/7.png', 2, 'در این قسمت تکنولوژی های بکار رفته قرار دارد.'),
+(27, 'assets/img/10.1.png', 2, 'این یک فرم ارسال رزومه است .'),
+(28, 'assets/img/9.1.png', 2, 'این صفحه پروژه های دنجی است .'),
+(29, 'assets/img/2.png', 2, ' این صفحه توضیحات درباره دنجی است.'),
+(30, 'assets/img/5.1.png', 2, 'نمونه کارهای دنجی در این صفحه است.'),
+(31, 'assets/img/11.1.png', 2, ''),
+(32, 'assets\\img\\1-a.jpg', 2, ''),
+(33, 'assets\\img\\2-a.jpg', 2, ''),
+(34, 'assets\\img\\3-a.jpg', 2, ''),
+(35, 'assets\\img\\4-a.jpg', 2, ''),
+(36, 'assets\\img\\5-a.jpg', 2, ''),
+(37, 'assets\\img\\6-a.jpg', 2, ''),
+(38, '', 0, ''),
+(39, 'assets/img/7.png', 1, 'در این قسمت تکنولوژی های بکار رفته قرار دارد.'),
+(40, 'assets/img/10.1.png', 1, 'این یک فرم ارسال رزومه است .'),
+(41, 'assets/img/9.1.png', 1, 'این صفحه پروژه های دنجی است .'),
+(42, 'assets/img/2.png', 1, ' این صفحه توضیحات درباره دنجی است.'),
+(43, 'assets/img/5.1.png', 1, ' نمونه کارهای دنجی در این صفحه است.'),
+(44, 'assets/img/11.1.png', 1, 'در این صفحه رضایت مشتری قرار دارد .'),
+(45, 'assets/img/7.png', 2, 'در این قسمت تکنولوژی های بکار رفته قرار دارد.'),
+(46, 'assets/img/10.1.png', 2, 'این یک فرم ارسال رزومه است .'),
+(47, 'assets/img/9.1.png', 2, 'این صفحه پروژه های دنجی است .'),
+(48, 'assets/img/2.png', 2, ' این صفحه توضیحات درباره دنجی است.'),
+(49, 'assets/img/5.1.png', 2, 'نمونه کارهای دنجی در این صفحه است.'),
+(50, 'assets/img/11.1.png', 2, ''),
+(51, 'assets\\img\\1-a.jpg', 2, ''),
+(52, 'assets\\img\\2-a.jpg', 2, ''),
+(53, 'assets\\img\\3-a.jpg', 2, ''),
+(54, 'assets\\img\\4-a.jpg', 2, ''),
+(55, 'assets\\img\\5-a.jpg', 2, ''),
+(56, 'assets\\img\\6-a.jpg', 2, ''),
+(57, '', 0, '');
+
 -- --------------------------------------------------------
 
 --
@@ -85,6 +147,20 @@ CREATE TABLE `languages` (
   `title` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
 
+--
+-- Dumping data for table `languages`
+--
+
+INSERT INTO `languages` (`id`, `title`) VALUES
+(1, 'HTML & CSS'),
+(2, 'Bootstrap'),
+(3, 'Javascript'),
+(4, 'PHP'),
+(5, 'Laravel'),
+(3, 'Javascript'),
+(4, 'PHP'),
+(5, 'Laravel');
+
 -- --------------------------------------------------------
 
 --
@@ -93,10 +169,20 @@ CREATE TABLE `languages` (
 
 CREATE TABLE `portfolio` (
   `id` int(11) NOT NULL,
-  `title` varchar(15) NOT NULL,
-  `picture_link` varchar(150) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `project_link` varchar(100) NOT NULL,
   `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `portfolio`
+--
+
+INSERT INTO `portfolio` (`id`, `title`, `project_link`, `description`) VALUES
+(1, ' پروژه دنجی', 'assets\\img\\Screenshot.png', 'این پروژه تیمی انجام شده با استفاده از html , css , javascript , bootstrap انجام شده .'),
+(2, 'پروژه بوت استرپ', 'assets\\img\\Screenshot (2).png', 'این پروژه تنها با فریم ورک Bootstrap 5 انجام شده .'),
+(4, ' پروژه دنجی', 'assets\\img\\Screenshot.png', 'این پروژه تیمی انجام شده با استفاده از html , css , javascript , bootstrap انجام شده .'),
+(6, 'پروژه HTML & CSS ', 'assetsimg1-a.jpg', 'طراحی جذاب و مدرن با HTML & CSS');
 
 -- --------------------------------------------------------
 
@@ -106,9 +192,19 @@ CREATE TABLE `portfolio` (
 
 CREATE TABLE `portfolio_contributor` (
   `id` int(11) NOT NULL,
-  `port_id` int(11) NOT NULL,
+  `portfolio_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `portfolio_contributor`
+--
+
+INSERT INTO `portfolio_contributor` (`id`, `portfolio_id`, `user_id`) VALUES
+(5, 1, 1),
+(6, 2, 1),
+(7, 4, 2),
+(8, 6, 2);
 
 -- --------------------------------------------------------
 
@@ -121,6 +217,20 @@ CREATE TABLE `portfolio_languages` (
   `portfolio_id` int(11) NOT NULL,
   `language_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+
+--
+-- Dumping data for table `portfolio_languages`
+--
+
+INSERT INTO `portfolio_languages` (`id`, `portfolio_id`, `language_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 2, 2),
+(5, 4, 1),
+(6, 4, 2),
+(7, 4, 3),
+(8, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -148,21 +258,33 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `name`, `resume`, `j
 (1, 'raha', 'rahasdi@gmail.com', 'raha8111', 'فاطمه سرحدی', 'assets/pdf/resume.pdf', 'Web Developer', NULL, 'assets/img/DSC3414.jpg'),
 (2, 'Anahita', 'anahitanadi.99@gmail.com', 'ana8111', 'آناهیتا نادی', 'assets/pdf/resume_anahita.pdf', 'Web Developer', NULL, 'assets/img/DSC3423.jpg');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user_language`
---
-
-CREATE TABLE `user_language` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `language_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
-
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `galleries`
+--
+ALTER TABLE `galleries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `portfolio_contributor`
+--
+ALTER TABLE `portfolio_contributor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `portfolio_languages`
+--
+ALTER TABLE `portfolio_languages`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -174,6 +296,30 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `galleries`
+--
+ALTER TABLE `galleries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+
+--
+-- AUTO_INCREMENT for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `portfolio_contributor`
+--
+ALTER TABLE `portfolio_contributor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `portfolio_languages`
+--
+ALTER TABLE `portfolio_languages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
