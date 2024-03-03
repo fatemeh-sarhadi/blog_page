8dc6b3c3b103e1e8efc7e5a6582c988578449aa0
anahita-nadi <anahitanadi.99@gmail.com>
2024-03-03 11:23:10 +0330

    [EDIT] css style link
     By Anahita Nadi

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../assets/css/pages_style/home.css">
+  <link rel="stylesheet" href="./assets/css/pages_style/home.css">
   <link rel="stylesheet" href="./assets/css/bootstrap-icons.css">
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
   <script src="./assets/js/bootstrap.bundle.min.js"></script>
