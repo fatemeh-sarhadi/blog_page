965f74c55b4bdc9c3c97dd7b376fa1cb7b795671
anahita-nadi <anahitanadi.99@gmail.com>
2024-03-06 12:25:47 +0330

    [edit] admin page, add img
    By Anahita nadi

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,4 +5,36 @@ document.querySelector('#contact-form').addEventListener('submit', (e) => {
     e.target.elements.message.value = '';
   });
 
-  
+  var to, from;
+to = $(".range-to-example").persianDatepicker({
+    inline: true,
+    altField: '.range-to-example-alt',
+    altFormat: 'LLLL',
+    initialValue: false,
+    onSelect: function (unix) {
+        to.touched = true;
+        if (from && from.options && from.options.maxDate != unix) {
+            var cachedValue = from.getState().selected.unixDate;
+            from.options = {maxDate: unix};
+            if (from.touched) {
+                from.setDate(cachedValue);
+            }
+        }
+    }
+});
+from = $(".range-from-example").persianDatepicker({
+    inline: true,
+    altField: '.range-from-example-alt',
+    altFormat: 'LLLL',
+    initialValue: false,
+    onSelect: function (unix) {
+        from.touched = true;
+        if (to && to.options && to.options.minDate != unix) {
+            var cachedValue = to.getState().selected.unixDate;
+            to.options = {minDate: unix};
+            if (to.touched) {
+                to.setDate(cachedValue);
+            }
+        }
+    }
+});
